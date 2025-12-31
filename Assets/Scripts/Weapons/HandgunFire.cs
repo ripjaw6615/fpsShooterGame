99354f5688bcbd78567b3bbe4b69556ca5b319ea
ripjaw6615 <shahrishi2901@gmail.com>
2025-12-31 19:29:21 +0530

    shooting mechanism added(gun animation, firing sound and firing script)

--- Assets/Scripts/Weapons/HandgunFire.cs
+++ Assets/Scripts/Weapons/HandgunFire.cs
@@ -1,13 +1,34 @@
 using UnityEngine;
+using System.Collections;
+
 
 public class HandgunFire : MonoBehaviour
 {
     [SerializeField] AudioSource gunfire;
+    [SerializeField] GameObject handgun;
+    [SerializeField] bool canFire=true;
     void Update()
     {
         if(Input.GetMouseButton(0))
         {
-            gunfire.Play();
+            if(canFire==true)
+            {
+                canFire=false;
+                StartCoroutine(FiringGun());
+
+            }
         }
     }
+
+
+IEnumerator FiringGun()
+{
+    gunfire.Play();
+    handgun.GetComponent<Animator>().Play("HandgunFire");
+    yield return new WaitForSeconds(0.25f);
+    handgun.GetComponent<Animator>().Play("New State");
+    yield return new WaitForSeconds(0.05f);
+    canFire=true;
+}
+
 }
